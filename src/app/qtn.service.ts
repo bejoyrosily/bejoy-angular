8d73f1110a5ba1aead68c90c6697f67f22db635f
bejoyrosily <bejoyrosily@gmail.com>
2018-11-06 18:46:56 +0530

    something2 commit

--- src/app/qtn.service.ts
+++ src/app/qtn.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class QtnService {
 
-  private questionsUrl = 'https://serene-wildwood-19977.herokuapp.com/';
+  private questionsUrl = 'https://serene-wildwood-19977.herokuapp.com';
 
   getQtns(): Observable<Question[]>{
      
@@ -17,20 +17,20 @@ export class QtnService {
 
 
   addQtns(qtn:Question): Observable<Question>{
-    return this.http.post<Question>(this.questionsUrl+'/add',qtn);
+    return this.http.post<Question>(this.questionsUrl+'/api/questions/add',qtn);
   };
 
   deleteQtn(id:string):Observable<Question>{
-    return this.http.delete<Question>(this.questionsUrl+`/delete/${id}`);
+    return this.http.delete<Question>(this.questionsUrl+`/api/questions/delete/${id}`);
   }
 
   getQtn(id:string):Observable<Question>{
      
-    return this.http.get<Question>(this.questionsUrl+`/edit/${id}`);
+    return this.http.get<Question>(this.questionsUrl+`/api/questions/edit/${id}`);
     };
 
   updateQtn(qtnn:Question): Observable<Question>{
-    return this.http.put<Question>(this.questionsUrl+`/edit/${qtnn._id}`,qtnn);
+    return this.http.put<Question>(this.questionsUrl+`/api/questions/edit/${qtnn._id}`,qtnn);
   };
 
   constructor(private http:HttpClient) { }
